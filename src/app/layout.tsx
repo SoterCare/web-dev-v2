288db988879d61074ec985f49c2c07a5ad5d6921
Daham Dissanayake <dahamdissanayake05@gmail.com>
2026-01-17 18:50:31 +0530

    Minor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SoterCare",
+  title: "SoterCare.",
   description: "IoT & ML Based Health Monitoring System",
 };
 
