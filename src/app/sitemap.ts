0dfa73cdbe3007b9c1a1690de105ad9ed325c8e0
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-01-21 15:55:13 +0530

    fixed sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,12 +2,12 @@ import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://sotercare.com";
-  const lastModified = new Date();
+  
 
   return [
     {
       url: baseUrl,
-      lastModified,
+      lastModified : "2026-01-19",
       changeFrequency: "weekly",
       priority: 1.0,
     },
